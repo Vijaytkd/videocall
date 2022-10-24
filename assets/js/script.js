f0e21ffc9266db1f888c5acaa06b38906211b317
Vijaytkd <97897558+Vijaytkd@users.noreply.github.com>
2022-10-24 23:04:40 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,153 @@
+// Generate random room name if needed
+if (!location.hash) {
+    location.hash = Math.floor(Math.random() * 0xFFFFFF).toString(16);
+  }
+  const roomHash = location.hash.substring(1);
+  // console.log(roomHash);
+  
+  // TODO: Replace with your own channel ID
+  const drone = new ScaleDrone('LRg6dUR0yUrZUTjW');
+  // Room name needs to be prefixed with 'observable-'
+  const roomName = 'observable-' + roomHash;
+  const configuration = {
+    iceServers: [{
+      urls: 'stun:stun.l.google.com:19302'
+    }]
+  };
+  let room;
+  let pc;
+  
+  
+  function onSuccess() {};
+  function onError(error) {
+    console.error(error);
+  };
+  
+  drone.on('open', error => {
+    if (error) {
+      return console.error(error);
+    }
+    room = drone.subscribe(roomName);
+    room.on('open', error => {
+      if (error) {
+        onError(error);
+      }
+    });
+    // We're connected to the room and received an array of 'members'
+    // connected to the room (including us). Signaling server is ready.
+    room.on('members', members => {
+      console.log('MEMBERS', members);
+      // if (members.length >= 3) {//if more than 2 user in a room - new code
+      //   return alert('The room is full');//if more than 2 user in a room - new code
+      // }//if more than 2 user in a room - new code
+      // If we are the second user to connect to the room we will be creating the offer
+      const isOfferer = members.length === 2;
+      startWebRTC(isOfferer);
+    });
+  });
+  
+  // Send signaling data via Scaledrone
+  function sendMessage(message) {
+    drone.publish({
+      room: roomName,
+      message
+    });
+  }
+  
+  function startWebRTC(isOfferer) {
+    pc = new RTCPeerConnection(configuration);
+  
+    // 'onicecandidate' notifies us whenever an ICE agent needs to deliver a
+    // message to the other peer through the signaling server
+    pc.onicecandidate = event => {
+      if (event.candidate) {
+        sendMessage({'candidate': event.candidate});
+      }
+    };
+  
+    // If user is offerer let the 'negotiationneeded' event create the offer
+    if (isOfferer) {
+      pc.onnegotiationneeded = () => {
+        pc.createOffer().then(localDescCreated).catch(onError);
+      }
+    }
+  
+    // When a remote stream arrives display it in the #remoteVideo element
+    pc.ontrack = event => {
+      const stream = event.streams[0];
+      if (!remoteVideo.srcObject || remoteVideo.srcObject.id !== stream.id) {
+        remoteVideo.srcObject = stream;
+      }
+    };
+  
+    navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true,
+    }).then(stream => {
+      // Display your local video in #localVideo element
+      localVideo.srcObject = stream;
+      // Add your stream to be sent to the conneting peer
+      stream.getTracks().forEach(track => pc.addTrack(track, stream));
+      // =====================resume/pause video====================
+      var video_toggle=document.querySelector(".pause");
+      video_toggle.onclick=function()
+      {
+        stream.getVideoTracks()[0].enabled=!(stream.getVideoTracks()[0].enabled);//video pause/resume function
+        if(stream.getVideoTracks()[0].enabled==true){//checking if users video paused/not
+          $("#pause").addClass('fa-video');
+          $("#pause").removeClass('fa-video-slash');
+        }else{
+          $("#pause").addClass('fa-video-slash');
+          $("#pause").removeClass('fa-video');
+        }
+      }
+      // ===================resume/pause video======================
+      // =========================mute/unmute audio=============================
+      var audio_toggle=document.querySelector(".mute");
+      audio_toggle.onclick=function()
+      {
+        stream.getAudioTracks()[0].enabled=!(stream.getAudioTracks()[0].enabled);
+        if(stream.getAudioTracks()[0].enabled==true){
+          $("#mute").addClass('fa-microphone');
+          $("#mute").removeClass('fa-microphone-slash');
+          //$("#color").css("background-color","red");
+        }else{
+          $("#mute").addClass('fa-microphone-slash');
+          $("#mute").removeClass('fa-microphone');
+          //$("#color").css("background-color","yellow");
+        }
+      }
+      // =========================mute/unmute audio=============================
+    }, onError);
+  
+    // Listen to signaling data from Scaledrone
+    room.on('data', (message, client) => {
+      // Message was sent by us
+      if (client.id === drone.clientId) {
+        return;
+      }
+  
+      if (message.sdp) {
+        // This is called after receiving an offer or answer from another peer
+        pc.setRemoteDescription(new RTCSessionDescription(message.sdp), () => {
+          // When receiving an offer lets answer it
+          if (pc.remoteDescription.type === 'offer') {
+            pc.createAnswer().then(localDescCreated).catch(onError);
+          }
+        }, onError);
+      } else if (message.candidate) {
+        // Add the new ICE candidate to our connections remote description
+        pc.addIceCandidate(
+          new RTCIceCandidate(message.candidate), onSuccess, onError
+        );
+      }
+    });
+  }
+  
+  function localDescCreated(desc) {
+    pc.setLocalDescription(
+      desc,
+      () => sendMessage({'sdp': pc.localDescription}),
+      onError
+    );
+  }
---
+++ videocall.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="theme-color" content="#e21d27">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+<link rel="stylesheet" href="assets/css/style.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.3/css/lightbox.css">
+<link rel="stylesheet" href="assets/css/owl.carousel.css">
+<link rel="stylesheet" href="assets/css/owl.theme.default.css">
+<link rel="stylesheet" href="assets/css/sign.css">
+<link rel="stylesheet" href="assets/css/banner.css">
+<link rel="stylesheet" href="assets/css/responsive.css">
+<link rel="stylesheet" href="assets/css/profiledetail.css">
+<link rel="stylesheet" href="assets/css/header.css">
+<link rel="stylesheet" href="assets/css/slidemenu.css">
+
+<link rel="stylesheet" href="assets/css/croppie.css">
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-215829100-1">
+</script>
+
+<title>Video Call</title>
+</head>
+  <link rel="stylesheet" href="assets/css/videocall/style.css">
+  <script type='text/javascript' src='https://cdn.scaledrone.com/scaledrone.min.js'></script>
+  <title>Cupid Video Chat</title>
+</head>
+<body style="background-image:url('assets/css/videocall/heart.png');">
+  <div class="container">
+    <video src="" id="localVideo" class="localVideo" autoplay></video>
+    <video src="" id="remoteVideo" class="remoteVideo" autoplay></video>
+    <button type="button" id="hung" class="btn btn-danger btn-sm hang"><i class=" fas fa-phone-slash"></i></button>
+    <button type="button" class="btn btn-secondary btn-sm mute"><i id="mute" class="fas fa-microphone"></i></button>
+    <button type="button" class="btn btn-secondary btn-sm pause"><i id="pause" class="fas fa-video"></i></button>
+  </div>
+</body>
+<script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
+<script src="assets/js/script.js"></script>
+</html>
